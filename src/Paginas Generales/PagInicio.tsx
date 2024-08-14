a3f3cc1f38c7d74a26853bfd54f020ba5fbbe8ee
Lucasszejn <roguenine6@gmail.com>
2024-08-14 00:50:06 -0300

    diseño

--- src/Paginas Generales/PagInicio.tsx
+++ src/Paginas Generales/PagInicio.tsx
@@ -6,24 +6,29 @@ const PagInicio: React.FC = () => {
   return (
     <div className="PagInicio-Container">
       <header className="PagInicio-Header">
-        <img src="./logo.jpg" alt="Mi Marca" className="PagInicio-Logo" /> {/* Ruta directa para el logo */}
+        <Link to="/">
+          <img src="./logo.jpg" alt="Mi Marca" className="PagInicio-Logo" />
+        </Link>
         <h1 className="PagInicio-Title">Página de Joseph Fiter</h1>
       </header>
       <div className="PagInicio-ButtonContainer">
-        <Link to="/mensajes" className="PagInicio-Button" style={{ backgroundImage: 'url(/path-to-your-image/mensajes.png)' }}>
-          Mensajes
+        <Link to="/mensajes" className="PagInicio-Button">
+          <img src="./shabatboton.png" alt="Mensajes" />
         </Link>
-        <Link to="/juegos" className="PagInicio-Button" style={{ backgroundImage: 'url(/path-to-your-image/juegos.png)' }}>
-          Juegos
+        <Link to="/juegos" className="PagInicio-Button">
+          <img src="./juegosboton.png" alt="Juegos" />
         </Link>
-        <Link to="/mods" className="PagInicio-Button" style={{ backgroundImage: 'url(/path-to-your-image/mods.png)' }}>
-          Mods
+        <Link to="/youtube" className="PagInicio-Button">
+          <img src="./youtubeboton.png" alt="youtube" />
         </Link>
-        <Link to="/rewind" className="PagInicio-Button" style={{ backgroundImage: 'url(/path-to-your-image/rewind.png)' }}>
-          Rewind DM
+        <Link to="/mods" className="PagInicio-Button">
+          <img src="./modsboton.png" alt="Mods" />
         </Link>
-        <Link to="/mas" className="PagInicio-Button" style={{ backgroundImage: 'url(/path-to-your-image/mas.png)' }}>
-          Más
+        <Link to="/rewind" className="PagInicio-Button">
+          <img src="./rewindboton.png" alt="Rewind DM" />
+        </Link>
+        <Link to="/mas" className="PagInicio-Button">
+          <img src="./masboton.png" alt="Más" />
         </Link>
       </div>
     </div>
