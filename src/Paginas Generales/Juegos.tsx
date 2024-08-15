3e43d009b16e31e83cf749df685051e517373f23
Lucasszejn <roguenine6@gmail.com>
2024-08-15 19:58:18 -0300

    dale

--- src/Paginas Generales/Juegos.tsx
+++ src/Paginas Generales/Juegos.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import FetchJuegos from '../componentes/RequestJuegos';
 
 const Juegos: React.FC = () => {
-  return <div>Juegos</div>;
+  return (
+    <div>
+      <h1>Juegos</h1>
+      <FetchJuegos />
+    </div>
+  );
 }
 
 export default Juegos;
---
+++ src/componentes/RequestJuegos.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const FetchJuegos: React.FC = () => {
+  useEffect(() => {
+    const fetchJuegos = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/juegos');
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching juegos:', error);
+      }
+    };
+
+    fetchJuegos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fetch Juegos</h1>
+      <p>Check the console for the fetched data.</p>
+    </div>
+  );
+};
+
+export default FetchJuegos;
