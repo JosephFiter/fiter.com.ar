ecb80a6e820a8f1a3f426f36ff0170fcdd2a57f5
Lucasszejn <roguenine6@gmail.com>
2024-08-16 23:10:44 -0300

    juegos

--- src/Paginas Generales/Juegos.tsx
+++ src/Paginas Generales/Juegos.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FetchJuegos from '../componentes/RequestJuegos';
-
+import './css de generales/Juegos.css';
 const Juegos: React.FC = () => {
   return (
     <div>
-      <h1>Juegos</h1>
+       <header className="PagInicio-Header">
+        <Link to="/">
+          <img src="./logo.jpg" alt="Mi Marca" className="PagInicio-Logo" />
+        </Link>        
+        <Link to="/">
+        <img src="./botonVolver.png" alt="Mensajes" className='Juegos-Volver' />
+        </Link>
+      </header>
       <FetchJuegos />
     </div>
   );
--- src/componentes/RequestJuegos.tsx
+++ src/componentes/RequestJuegos.tsx
@@ -5,7 +5,7 @@ const FetchJuegos: React.FC = () => {
   useEffect(() => {
     const fetchJuegos = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/juegos');
+        const response = await axios.get('https://backjoseph-com-ar2024.vercel.app/juegos');
         console.log(response.data);
       } catch (error) {
         console.error('Error fetching juegos:', error);
